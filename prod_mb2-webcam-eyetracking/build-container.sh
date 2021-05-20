e5ebef1d18c562e917ad5ac3e4e0628fd3b9a415
adriansteffan <adrian.steffan@hotmail.de>
2021-05-21 01:29:55 +0200

    Fix docker setup - buildcontainer script

--- prod_mb2-webcam-eyetracking/build-container.sh
+++ prod_mb2-webcam-eyetracking/build-container.sh
@@ -3,8 +3,8 @@ set -e
 
 cd ..
 ./build.sh
-cp -r local-server/webroot prod
-cd prod
+cp -r local-server/webroot prod_mb2-webcam-eyetracking
+cd prod_mb2-webcam-eyetracking
 docker-compose build --no-cache
 docker-compose up -d
 chown 33:33 -R data
