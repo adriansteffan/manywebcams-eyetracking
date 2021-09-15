6d8c78c5e56b38141d53d7e6e9c27000e0955e05
adriansteffan <adrian.steffan@hotmail.de>
2021-09-15 12:16:42 +0200

    Rework data upload to include feedback for the user

--- src/write_data.php
+++ src/write_data.php
@@ -1,4 +1,5 @@
 <?php
+
 $post_data = json_decode(file_get_contents('php://input'), true);
 $filename_no_newlines = str_replace(array("\n", "\r"), '', $post_data['filename']);
 $sanitized_filename = preg_replace('/[^\da-z_]/i', '', $filename_no_newlines);
@@ -8,16 +9,22 @@ $sanitized_filetype = '';
 if ($filetype === 'json' || $filetype === 'csv') {
     $data = $post_data['filedata'];
     $sanitized_filetype = $filetype;
-
-} else if ($filetype === 'webm') { 
+} else if ($filetype === 'webm') {
     $data = base64_decode(explode(',', $post_data['filedata'])[1], true);
     $sanitized_filetype = 'webm';
-
 } else {
+    http_response_code(400);
     die();
 }
 
+
 $name = '/var/www/data/' . $sanitized_filename . '.' .  $sanitized_filetype;
 
-file_put_contents($name, $data);
+while (file_exists($name)) {
+    $name = $name . '-' . time() . '-' . uniqid();
+}
+
+if (!file_put_contents($name, $data)) {
+    http_response_code(500);
+}
 ?>
