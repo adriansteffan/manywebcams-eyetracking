b32b3cd56deed3359a8fa660fb34cc8037ec432c
adriansteffan <adrian.steffan@hotmail.de>
2021-10-21 16:33:12 +0200

    Fix index.html renaming

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 cp -r jspsych-6.3.1 local-server/webroot
-cp src/experiment.html local-server/webroot/index.html
-cp src/patch.css local-server/webroot/patch.css
-cp src/write_data.php local-server/webroot/write_data.php
-cp src/jszip.min.js local-server/webroot/jszip.min.js
-
+cp -r src/* local-server/webroot
 cp -r media local-server/webroot
+
+mv local-server/webroot/experiment.html local-server/webroot/index.html
