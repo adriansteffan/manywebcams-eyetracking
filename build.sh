e69f42ad109a9306850c567d29bf50dea333ff02
adriansteffan <adrian.steffan@hotmail.de>
2021-10-21 16:12:03 +0200

    Add jszip to build script

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ cp -r jspsych-6.3.1 local-server/webroot
 cp src/experiment.html local-server/webroot/index.html
 cp src/patch.css local-server/webroot/patch.css
 cp src/write_data.php local-server/webroot/write_data.php
+cp src/jszip.min.js local-server/webroot/jszip.min.js
+
 cp -r media local-server/webroot
