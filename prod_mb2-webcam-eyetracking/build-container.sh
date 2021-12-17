f737b0746723518a1eea75a72711d360828d0b38
adriansteffan <adrian.steffan@hotmail.de>
2021-12-18 00:10:14 +0100

    Fix deployment for certain setups

--- prod_mb2-webcam-eyetracking/build-container.sh
+++ prod_mb2-webcam-eyetracking/build-container.sh
@@ -8,4 +8,3 @@ cp -r local-server/webroot prod_mb2-webcam-eyetracking
 cd prod_mb2-webcam-eyetracking
 docker-compose build --no-cache
 docker-compose up -d
-chown 33:33 -R data
---
+++ prod_mb2-webcam-eyetracking/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -d "/var/www/data" ]; then
+    echo "/var/www/data does not exist. Is the folder mount setup correctly in docker-compose.yml?"
+    exit 1
+fi
+
+chown -R www-data:www-data /var/www/data
+
+apachectl -D FOREGROUND
